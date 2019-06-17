de0798028c9f16168a1a5713a0065b55cd09caa1
Seth NeJame <sethnejame@gmail.com>
2019-06-17 11:44:38 +0200

    Adding working HTML/JS button interaction

--- js/script.js
+++ js/script.js
@@ -2,4 +2,16 @@
 let jokeArray = ["Why was the the duck arrested?  Because he was caught selling quack!",
                 "What do you call a fish with two knees? A two-knee-fish!",
                 "What do you call a chimpanzee who works with pottery?  Harry Potter!"
-                ]
+                ];
+
+let today = new Date();
+
+console.log(today);
+
+function tellJoke() {
+    document.getElementById("output").innerHTML = (jokeArray[0])
+};
+
+function giveDate() {
+    document.getElementById("output").innerHTML = (today)
+};
