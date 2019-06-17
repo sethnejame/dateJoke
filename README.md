8efc8332d23173c188c393ae6c4dca657288f945
Seth NeJame <sethnejame@gmail.com>
2019-06-17 12:22:59 +0200

    Adding README.md, fully functioning JS buttons

---
+++ README.md
@@ -0,0 +1,17 @@
+# Joke & Date Website Exercise
+### Colloqially known as the greatest website ever created
+
+Per Faraz's specs, the attached website contains two buttons:
+
+1. One button, when pressed, displays today's date, formatted
+2. The other button, when pressed, displays a random joke from an array
+
+The website can be viewed in your browser by visiting https://sethnejame.github.io/jokeDate
+
+Learning resources:
+
+* W3 Schools - The Date Object, JS Functions, OnClick
+https://www.w3schools.com/js/js_dates.asp
+https://www.w3schools.com/jsref/event_onclick.asp
+https://www.w3schools.com/jsref/jsref_random.asp
+
--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 
+// random list of bad jokes, courtesy of Seth
 let jokeArray = ["Why was the the duck arrested?  Because he was caught selling quack!",
                 "What do you call a fish with two knees? A two-knee-fish!",
                 "What do you call a chimpanzee who works with pottery?  Harry Potter!"
                 ];
 
+// get current date with Date object
 let today = new Date();
 
-console.log(today);
-
 function tellJoke() {
-    document.getElementById("output").innerHTML = (jokeArray[0])
+    // use Math.random, mult by the length of the jokeArray, use that position in jokeArray index for rando joke
+    let randomJoke = jokeArray[Math.floor(Math.random()*jokeArray.length)]
+    // grab inner html of output div, replace with joke
+    document.getElementById("output").innerHTML = (randomJoke)
 };
 
 function giveDate() {
-    document.getElementById("output").innerHTML = (today)
+    // grab inner html of output div, replace with joke
+    document.getElementById("output").innerHTML = (today.toDateString())
 };
