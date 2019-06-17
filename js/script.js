fdb2e90e266385df48fcac7ddf5bb13aab7905c2
Seth NeJame <sethnejame@gmail.com>
2019-06-17 11:10:03 +0200

    Initial file structure and basic skeleton for Date & Joke exercise

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+let jokeArray = ["Why was the the duck arrested?  Because he was caught selling quack!",
+                "What do you call a fish with two knees? A two-knee-fish!",
+                "What do you call a chimpanzee who works with pottery?  Harry Potter!"
+                ]
